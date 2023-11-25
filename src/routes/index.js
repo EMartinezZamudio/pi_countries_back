fb55f03ec10ecdfc377a693b5efdb7f16670242b
Emmanuel <manumtz20@gmail.com>
2023-11-24 18:36:27 -0600

    deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("prueba de api");
+  res.send("prueba de api nueva");
 });
 
 module.exports = router;
